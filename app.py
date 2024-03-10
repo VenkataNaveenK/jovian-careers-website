cd3d91cdea387f0c1e71f7c25f6d592276a63682
VenkataNaveenK <venkatanaveek5@gmail.com>
2024-03-10 22:08:11 +0530

    added connection to db

--- app.py
+++ app.py
@@ -1,41 +1,44 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': "Data Scientist",
-        'location': 'Bengaluru, India',
-        'salary': '10,00,000'
-    },
-    {
-        'id': 2,
-        'title': "Data Analyst",
-        'location': 'Pune, India',
-        'salary': '8,00,000'
-    },
-    {
-        'id': 3,
-        'title': "Python Developer",
-        'location': 'Hyderabad, India'
-    },
-    {
-        'id': 4,
-        'title': "ML Engineer",
-        'location': 'Delhi, India',
-        'salary': '15,00,000'
-    }
-]
+# JOBS = [
+#     {
+#         'id': 1,
+#         'title': "Data Scientist",
+#         'location': 'Bengaluru, India',
+#         'salary': '10,00,000'
+#     },
+#     {
+#         'id': 2,
+#         'title': "Data Analyst",
+#         'location': 'Pune, India',
+#         'salary': '8,00,000'
+#     },
+#     {
+#         'id': 3,
+#         'title': "Python Developer",
+#         'location': 'Hyderabad, India'
+#     },
+#     {
+#         'id': 4,
+#         'title': "ML Engineer",
+#         'location': 'Delhi, India',
+#         'salary': '15,00,000'
+#     }
+# ]
 
 @app.route("/")
 def helloworld():
+    JOBS = load_jobs_from_db()
     return render_template("home.html", 
                            jobs=JOBS,
                            company_name="Jovian")
 
 @app.route("/api/jobs")
 def list_jobs():
+    JOBS = load_jobs_from_db()
     return jsonify(JOBS)
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, text
+import os
+
+db_name = "jovian_careers"
+db_connection_string =  os.environ['LOCAL_DB_CONNECTION_STRING']+db_name+"?charset=utf8mb4"
+
+engine = create_engine(db_connection_string,
+                    ## FOR SSL CERTIFICATE WE NEED TO CHECK DATABASE PROVIDER 
+                    #  connect_args={
+                    #     "ssl": {
+                    #         "ca": "/home/gord/client-ssl/ca.pem",
+                    #         "cert": "/home/gord/client-ssl/client-cert.pem",
+                    #         "key": "/home/gord/client-ssl/client-key.pem"
+                    #     }
+                    #  }
+                    )
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row._asdict())
+        
+        return jobs
