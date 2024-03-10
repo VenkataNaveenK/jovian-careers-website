9e3c0f0732ca06ead0e968fba69392866231182b
VenkataNaveenK <venkatanaveek5@gmail.com>
2024-03-10 13:58:04 +0530

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-    return "Hello, Naveen"
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True) # '0.0.0.0' is used to run local and debug to True for running continuously
-    
